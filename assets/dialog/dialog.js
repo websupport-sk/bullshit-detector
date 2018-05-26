3d282a4f69dc752e92dcb7070f47830ede7bc99f
PetrToman <1likeGIT>
2018-05-26 16:20:01 +0200

    Localization support + added Czech language

--- assets/dialog/dialog.js
+++ assets/dialog/dialog.js
@@ -2,7 +2,21 @@ var link_report = "https://www.konspiratori.sk/nahlasenie-stranky.php";
 var link_about = "https://www.websupport.sk/bullshit-detector";
 var link_konspiratori = "https://www.konspiratori.sk/";
 var link_websupport = "https://www.websupport.sk/";
-document.getElementById("report").href = link_report;
-document.getElementById("about").href = link_about;
-document.getElementById("konspiratori").href = link_konspiratori;
-document.getElementById("websupport").href = link_websupport;
+
+function setLink(elementId, url) {
+    document.getElementById(elementId).href = url;
+}
+
+function setText(key) {
+    document.getElementById(key).innerHTML = chrome.i18n.getMessage(key);
+}
+
+setLink("dialog_report", link_report);
+setLink("dialog_about", link_about);
+setLink("dialog_konspiratori", link_konspiratori);
+setLink("dialog_websupport", link_websupport);
+
+setText("dialog_doubts");
+setText("dialog_check");
+setText("dialog_report");
+setText("dialog_about");
