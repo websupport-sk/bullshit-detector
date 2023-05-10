966061f34614dcfdcf6a630eeab11a8a536982f0
matusporuben <matus.poruben@gmail.com>
2023-05-10 18:40:36 +0200

    use vite for building

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import {defineConfig} from 'vite';
+
+
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    outDir: './dist',
+    lib: {
+      name: 'bullshit-detektor',
+      entry: './src/background.ts',
+      formats: ['es'],
+      fileName: (format: string) => `background.${format}.js`,
+    },
+    rollupOptions: {
+      treeshake: 'safest',
+      output: {},
+    },
+  },
+})
